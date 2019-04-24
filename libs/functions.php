5231d3bf3504e7a0adb749d1ee239ce69324bb73
Хмелинин Дмитрий <dh@webmotor.ru>
2019-04-24 17:49:28 +0500

    update function debug

--- libs/functions.php
+++ libs/functions.php
@@ -3,20 +3,26 @@
  * Khmelinin Dmitry
  */
 
-function debug ($arr) {
+function debug($arr, $die = false)
+{
     echo '<pre>' . print_r($arr, true) . '</pre>';
+    if ($die) {
+        die;
+    }
 }
 
-function redirect ($http = false) {
-    if($http) {
+function redirect($http = false)
+{
+    if ($http) {
         $redirect = $http;
-    }else{
+    } else {
         $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PATH;
     }
     header("Location: $redirect");
     exit;
 }
 
-function h($str) {
+function h($str)
+{
     return htmlspecialchars($str, ENT_QUOTES);
 }
